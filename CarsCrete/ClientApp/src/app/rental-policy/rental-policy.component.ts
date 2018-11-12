3c29a11558b4566298c5add7d2a98645566fa518
Andrew Ledashev <andres.ledachev@mail.ru>
2018-11-13 01:38:28 +0300

    Добавил в rental-policy.ts список и сделал его вывод в html (css доделать нужно)

--- CarsCrete/ClientApp/src/app/rental-policy/rental-policy.component.ts
+++ CarsCrete/ClientApp/src/app/rental-policy/rental-policy.component.ts
@@ -7,11 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rental-policy.component.css']
 })
 export class RentalPolicyComponent implements OnInit {
-
+  items:Rules[] = [
+    {
+      Name: "Collision damage waiver with ZERO EXCESS (CDW)",
+      Description: "The renter is insured for the rental vehicle damaged by accident with no obligation to pay any excess.",
+    },
+    {
+      Name: "Full collision damage waiver with ZERO EXCESS (FDW & WUG)",
+      Description: "The renter is insured for damages caused to the underside of the vehicle, glass, tires and engine with no obligation to pay any excess. It may also be referred as Super Collision damage waiver (SCDW) or Full collision damage waiver (FCDW) or Wheels under side and glass insurance (WUG).",
+    },
+    {
+      Name: "Theft Protection with ZERO EXCESS (TP)",
+      Description: "The renter is insured in case the vehicle is stolen, except if theft occurs because of negligence. Any stolen belongings in the interior of the car (eg. cameras, travel bags or cell-phones) are not covered by any insurance.",
+    },
+    {
+      Name: "Public Liability Insurance (PL)",
+      Description: "The renter’s liability is covered to a maximum of €1.000.000 for death and bodily injuries and €1.000.000 for material damages. May also referred as PLI.",
+    },
+    {
+      Name: "Personal accident insurance (PAI)",
+      Description: "Passengers of the rental vehicle are insured for death or bodily injuries in case of accident to a maximum of €1.000.000. Driver is insured to the amount of €15.000.",
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
     console.log('worker');
   }
 
+}
+
+export interface Rules {
+  Name: string;
+  Description: string;
 }
