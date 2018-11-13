9c4f9263d9843e4d93f73032fbd729afe89e079c
Andrew Ledashev <andres.ledachev@mail.ru>
2018-11-14 00:34:06 +0300

    Добавил в policy перевод из en и ru +  в них перевел что заголовки

--- CarsCrete/ClientApp/src/app/rental-policy/rental-policy.component.ts
+++ CarsCrete/ClientApp/src/app/rental-policy/rental-policy.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 
 @Component({
@@ -9,27 +10,37 @@ import { Component, OnInit } from '@angular/core';
 export class RentalPolicyComponent implements OnInit {
   items:Rules[] = [
     {
-      Name: "Collision damage waiver with ZERO EXCESS (CDW)",
-      Description: "The renter is insured for the rental vehicle damaged by accident with no obligation to pay any excess.",
+      Name: "Отказ от повреждений при столкновении с нулевым избытком (CDW).",
+      Description: "Арендатор застрахован на/от аренду автомобиля, поврежденного в результате аварии без обязательств по уплате !сверхплаты!",
+      NameEng: "Collision damage waiver with ZERO EXCESS (CDW)",
+      DescriptionEng: "The renter is insured for the rental vehicle damaged by accident with no obligation to pay any excess.",
     },
     {
-      Name: "Full collision damage waiver with ZERO EXCESS (FDW & WUG)",
-      Description: "The renter is insured for damages caused to the underside of the vehicle, glass, tires and engine with no obligation to pay any excess. It may also be referred as Super Collision damage waiver (SCDW) or Full collision damage waiver (FCDW) or Wheels under side and glass insurance (WUG).",
+      Name: "Полный отказ от повреждений при столкновении с нулевым избытком (FDW & WUG)",
+      Description: "Арендатор застрахованного не обязан доплачивать за ущерб, причиненный дну автомобиля, стеклу, шинам и двигателю. Это может также упоминаться как super collision damage waiver (SCDW) или full damage insurance (CDW) или wheel, bottom and glass insurance (GWP).",
+      NameEng: "Full collision damage waiver with ZERO EXCESS (FDW & WUG)",
+      DescriptionEng: "The renter is insured for damages caused to the underside of the vehicle, glass, tires and engine with no obligation to pay any excess. It may also be referred as Super Collision damage waiver (SCDW) or Full collision damage waiver (FCDW) or Wheels under side and glass insurance (WUG).",
     },
     {
-      Name: "Theft Protection with ZERO EXCESS (TP)",
-      Description: "The renter is insured in case the vehicle is stolen, except if theft occurs because of negligence. Any stolen belongings in the interior of the car (eg. cameras, travel bags or cell-phones) are not covered by any insurance.",
+      Name: "Защита от кражи с нулевым превышением (TP)",
+      Description: "Арендатор застрахован на случай угона автомобиля, за исключением случаев, когда угон происходит по неосторожности. Любые украденные вещи в салоне автомобиля (например. камеры, дорожные сумки или мобильные телефоны) не покрываются какой-либо страховкой.",
+      NameEng: "Theft Protection with ZERO EXCESS (TP)",
+      DescriptionEng: "The renter is insured in case the vehicle is stolen, except if theft occurs because of negligence. Any stolen belongings in the interior of the car (eg. cameras, travel bags or cell-phones) are not covered by any insurance.",
     },
     {
-      Name: "Public Liability Insurance (PL)",
-      Description: "The renter’s liability is covered to a maximum of €1.000.000 for death and bodily injuries and €1.000.000 for material damages. May also referred as PLI.",
+      Name: "Страхование гражданской ответственности (PL)",
+      Description: "Ответственность арендатора распространяется до €1.000.000 за смерть и телесные повреждения и €1.000.000 за материальный ущерб. Может также именоваться PLI.",
+      NameEng: "Public Liability Insurance (PL)",
+      DescriptionEng: "The renter’s liability is covered to a maximum of €1.000.000 for death and bodily injuries and €1.000.000 for material damages. May also referred as PLI.",
     },
     {
-      Name: "Personal accident insurance (PAI)",
-      Description: "Passengers of the rental vehicle are insured for death or bodily injuries in case of accident to a maximum of €1.000.000. Driver is insured to the amount of €15.000.",
+      Name: "Страхование от несчастных случаев (PAI)",
+      Description: "Пассажиры автомобиля застрахованы в случае смерти или телесных повреждений в случае аварии максимум €1.000.000. Водитель застрахован на сумму €15.000.",
+      NameEng: "Personal accident insurance (PAI)",
+      DescriptionEng: "Passengers of the rental vehicle are insured for death or bodily injuries in case of accident to a maximum of €1.000.000. Driver is insured to the amount of €15.000.",
     }
   ];
-  constructor() { }
+  constructor(public Translate:TranslateService) { }
 
   ngOnInit() {
   }
@@ -39,4 +50,6 @@ export class RentalPolicyComponent implements OnInit {
 export interface Rules {
   Name: string;
   Description: string;
+  NameEng: string;
+  DescriptionEng: string;
 }
